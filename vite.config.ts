8d27bacad6438e047448e4cb1ff3bcc01b0b2815
Lyes <lyes@MacBook-Air-de-Lyes.local>
2026-01-13 21:59:46 +0100

    mise à jour du site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/csf/',
   plugins: [
     react(),
     ViteImageOptimizer({
