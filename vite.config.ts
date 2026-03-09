5a12bef540ef942a4be154514f0cae4759c4b947
Lyes <lyes@MacBook-Air-de-Lyes.local>
2026-03-09 22:58:22 +0100

    Fix: change Vite base url to / for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/csf/',
+  base: '/',
   plugins: [
     react(),
     ViteImageOptimizer({
